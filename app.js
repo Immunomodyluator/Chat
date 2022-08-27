1bf1497849024334960ebf8ba68b5f70139d9727
Immunomodyluator <venhesterhack@gmail.com>
2022-08-27 15:34:35 +0300

    1

--- app.js
+++ app.js
@@ -1,41 +1,50 @@
-const express = require('express')
-const http = require('http')
-const { Server } = require("socket.io")
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-const app = express()
-const server = http.createServer(app)
-const io = new Server(server)
-const PORT = 80
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  /* options */
+});
+app.use(express.static("public"));
 
-app.use(express.static("public"))
+const PORT = 80;
 
-server.listen(PORT, () => {
-    console.log(`listening on: ${PORT}`)
-})
+let users = [];
 
-let users = []
+app.get("/hello", function (req, res) {
+  res.send("hello world");
+});
 
-io.on('connection', (socket) => {
-    let ID = (socket.id).toString().slice(1,6)
-    console.log(`${ID} connected`)
-    users.push({id: ID});
+io.on("connection", (socket) => {
+  let ID = socket.id.toString().slice(1, 6);
+  let IP = socket.handshake.address;
+  console.log(`${ID} connected`);
+  users.push({ id: ID, ip: IP });
+  console.log(users);
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', `${ID}: ${msg}`);
-        console.log('message: ' + `${ID}: ${msg}`);
-    })
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", `${ID}: ${msg}`);
+    console.log("message: " + `${ID}: ${msg}`);
+  });
 
-    socket.on('disconnect', () => {
-        let obIndex = users.findIndex(item => item.id === ID)
-        users.splice(obIndex, 1)
-        console.log(`${ID} disconnected`)
-    })
+  socket.on("disconnecting", () => {
+    let obIndex = users.findIndex((item) => item.id === ID);
+    users.splice(obIndex, 1);
+    onlineUser();
+    console.log(`${ID} disconnected`);
+  });
 
+  function onlineUser() {
     let usersHTML = "";
     for (let key in users) {
-        usersHTML = usersHTML + `<div>id ${users[key].id}</div>`
+      usersHTML = usersHTML + `<div>id ${users[key].id}</div>`;
     }
-    io.emit('usersHTML', usersHTML);
-})
+    io.emit("usersHTML", usersHTML);
+  }
+});
 
-
+httpServer.listen(PORT, () => {
+  console.log(`listening on: ${PORT}`);
+});
---
+++ public/script/script.js
@@ -0,0 +1,32 @@
+window.onload = function () {
+  let socket = io.connect('http://46.32.70.189/');
+  let messages = document.getElementById('chatHistory');
+  let online = document.getElementById('sideBarBody');
+
+  function textSend() {
+    let inputText = document.getElementById('chatInput__text');
+    socket.emit('chat message', inputText.textContent);
+    inputText.textContent = '';
+  }
+
+  document.getElementById('chatInput__svgWrap').addEventListener('click', () => {
+    textSend();
+  });
+  document.getElementById('chatInput__text').addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      textSend();
+    }
+  });
+
+  socket.on('usersHTML', function (usersHTML) {
+    online.innerHTML = usersHTML;
+  });
+
+  socket.on('chat message', function (msg) {
+    let item = document.createElement('div');
+    item.textContent = msg;
+    messages.appendChild(item);
+    messages.scrollTop = messages.scrollHeight;
+  });
+};
